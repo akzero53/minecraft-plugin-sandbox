4a0df411d8d90ba93e66693b4fed17ffe1a67028
Okuyama Eisuke <okuyama_eisuke@cyberagent.co.jp>
2017-02-03 14:53:13 +0900

    複数コマンドを登録するプラグインの作成検証用

--- src/main/java/com/github/akzero53/SandboxPlugin.java
+++ src/main/java/com/github/akzero53/SandboxPlugin.java
@@ -1,13 +1,17 @@
 package com.github.akzero53;
 
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
 /**
  * plugin.yml設定一覧ページ: http://minecraftjp.info/modding/index.php/Plugin.ymlの設定一覧
+ * SandboxPlugin is same as 'ProjectId'
+ *
  */
 public class SandboxPlugin extends JavaPlugin {
 
@@ -16,15 +20,53 @@ public class SandboxPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         logger.info("Minecraft Sandbox Plugin Enabled.");
+        this.getCommand("hoge").setExecutor(new HogeCommandExecutor());
+        this.getCommand("fuga").setExecutor(new FugaCommandExecutor());
+        this.getCommand("piyo").setExecutor(new PiyoCommandExecutor());
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        logger.info(cmd.getName());
+        Boolean isPlayer = sender instanceof Player;
+        logger.info("is sender player: " + isPlayer);
         return false;
     }
 
     @Override
     public void onDisable() {
         logger.info("Minecraft Sandbox Plugin Disabled.");
+    }
+
+    class HogeCommandExecutor implements CommandExecutor {
+//        private final SandboxPlugin plugin;
+//
+//        public HogeCommandExecutor(SandboxPlugin plugin) {
+//            this.plugin = plugin;
+//        }
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+            logger.info("hoge");
+            return false;
+        }
+    }
+
+    class FugaCommandExecutor implements CommandExecutor {
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+            logger.info("fuga");
+            return false;
+        }
+    }
+
+    class PiyoCommandExecutor implements CommandExecutor {
+
+        @Override
+        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+            logger.info("piyo");
+            return false;
+        }
     }
 }
